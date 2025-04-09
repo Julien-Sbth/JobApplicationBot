05c07a42b82a2d729be02bcea54bfdac36d7bede
Julien <julien.sabathe@ynov.com>
2025-04-09 20:43:23 +0200

    [ADD] Main Launch Program

---
+++ main.py
@@ -0,0 +1,9 @@
+from AutomationCore.orchestrator import LinkedInJobApplication
+
+def main():
+    print("🔵 Démarrage de l'automatisation LinkedIn...")
+    app = LinkedInJobApplication()
+    app.run()
+
+if __name__ == "__main__":
+    main()
